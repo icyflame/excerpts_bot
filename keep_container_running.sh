06fe9e23b459d1b70bfa3d7a7ff4e51a1c94d462
Siddharth Kannan <mail@siddharthkannan.in>
2020-06-28 09:00:54 +0900

    Rebuild image and restart container everyday

--- keep_container_running.sh
+++ keep_container_running.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
 # Schedule this script using crontab
-# 10 * * * * bash /home/siddharth/excerpts_bot/keep_container_running.sh
+# 0 0 * * * bash /home/siddharth/excerpts_bot/keep_container_running.sh
 
 cd /home/siddharth/excerpts_bot
 
 echo "START"
 date +"%F %R"
 
-docker container ls | grep -i excerpts > /dev/null
-if [[ "$?" != "0" ]];
-then
-        echo "Restarting container"
-        docker container stop excerpts
-        docker container rm excerpts
-        docker build . -t excerpts:latest
-        docker run -d --name excerpts excerpts:latest
-else
-        echo "Container is running, no need to restart"
-fi
+echo "STEP: Restarting container"
+
+docker container stop excerpts
+docker container rm excerpts
+docker build . -t excerpts:latest
+docker run -d --name excerpts excerpts:latest
+
+echo "STEP: Container restarted"
 
 date +"%F %R"
 echo "END"
